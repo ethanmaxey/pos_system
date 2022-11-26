33024ead2f6f1d3845e15d59d5bd9cf170fcad16
ethan <ethanm113@gmail.com>
2022-11-26 10:22:33 -0600

    fixed most of the buttons + product issue

--- posapp/models.py
+++ posapp/models.py
@@ -1,53 +1,15 @@
-from unittest.util import _MAX_LENGTH
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-from django.db import connections
-from django.contrib.auth.models import User
 
-# Create your models here.
-
-
-# class Employee(models.Model):
-#     emp_id = models.CharField('Employee ID', max_length=3)
-#     emp_name = models.CharField('Employee Name', max_length=50)
-#     emp_pay_rate = models.CharField('Pay Rate', max_length=6)
-#     emp_weekly_hours = models.DecimalField(
-#         'Weekly Hours', max_digits=3, decimal_places=1)
-#     emp_start_date = models.DateField('Employee Start Date')
-#     emp_dob = models.DateField('Date of Birth')
-#     emp_email = models.EmailField('Email Address')
-
-
-# class Customer(models.Model):
-#     cust_id = models.CharField('Customer ID', max_length=3)
-#     cust_name = models.CharField('Customer Name', max_length=50)
-#     cust_email = models.EmailField('Email Address')
-#     cust_dob = models.DateField("Customer's Date of Birth")
-
-
-# class PosappProduct(models.Model):
-#     product_id = models.IntegerField(primary_key=True)
-#     product_name = models.CharField(max_length=45)
-#     price = models.CharField(max_length=45)
-#     vendor_id = models.CharField(max_length=45)
-#     vendor_category = models.CharField(max_length=45)
-#     prod_amount = models.IntegerField()
-#     in_stock = models.CharField(max_length=45)
-
-#     class Meta:
-#         managed = False
-#         db_table = 'posapp_product'
-
-
-# # class Transactions(models.Model):
-# #     trans_id = models.CharField('Customer ID', max_length=3)
-# #     trans_cust_name = models.CharField('Customer Name', max_length=50)
-# #     trans_emp_id = models.EmailField('Email Address')
-# #     trans_date = models.DateField("Customer's Date of Birth")
-
-##############################################################
 
 class AuthGroup(models.Model):
-    name = models.CharField(unique=True, max_length=80)
+    name = models.CharField(unique=True, max_length=150)
 
     class Meta:
         managed = False
@@ -65,7 +27,7 @@ class AuthGroupPermissions(models.Model):
 
 
 class AuthPermission(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=255)
     content_type_id = models.IntegerField()
     codename = models.CharField(max_length=100)
 
@@ -82,7 +44,7 @@ class AuthUser(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
+    is_staff = models.IntegerField()
     is_active = models.IntegerField()
     date_joined = models.DateTimeField()
 
@@ -95,16 +57,29 @@ class Category(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.TextField()
     description = models.TextField()
-    date_added = models.DateField()
-    date_updated = models.DateField()
+    date_added = models.DateTimeField()
+    date_updated = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'category'
 
 
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
 class DjangoContentType(models.Model):
-    name = models.CharField(max_length=100)
     app_label = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
 
@@ -118,24 +93,34 @@ class DjangoMigrations(models.Model):
     id = models.BigAutoField(primary_key=True)
     app = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
-    applied = models.DateField()
+    applied = models.DateTimeField()
 
     class Meta:
         managed = False
         db_table = 'django_migrations'
 
 
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Products(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=24)
-    description = models.CharField(max_length=24)
+    name = models.TextField()
+    description = models.TextField()
     price = models.FloatField()
-    date_added = models.DateField()
-    date_updated = models.DateField()
-    category_id = models.ForeignKey('Category', models.DO_NOTHING)
+    date_added = models.DateTimeField()
+    date_updated = models.DateTimeField()
     amount = models.IntegerField()
     image = models.TextField(blank=True, null=True)
-    vendor_id = models.ForeignKey('Vendor', models.DO_NOTHING)
+    vendor_id = models.BigIntegerField(blank=True, null=True)
+    category_id = models.BigIntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
@@ -166,13 +151,3 @@ class Vendor(models.Model):
     class Meta:
         managed = False
         db_table = 'vendor'
-
-
-class VendorCategory(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    vendorid = models.ForeignKey(Vendor, models.DO_NOTHING, db_column='vendorid')
-    categoryid = models.ForeignKey(Category, models.DO_NOTHING, db_column='categoryid')
-
-    class Meta:
-        managed = False
-        db_table = 'vendor_category'
